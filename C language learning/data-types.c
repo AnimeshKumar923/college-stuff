94f82287c9eada223243cb2e0fcad7f4213af72b
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-05 22:57:08 +0530

    added a folder

---
+++ C language learning/data-types.c
@@ -0,0 +1,12 @@
+# include <stdlib.h>
+# include <stdio.h>
+
+int main()
+
+        int age = 40;
+        double gpa = 3.6;
+        char grade = 'A';
+        char phrase[] = "Giraffe Academy"
+
+        return 0;
+ 
---
+++ C language learning/hi.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello Universe!\n");
+    return 0;
+}
