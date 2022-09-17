8d75839da2f575334be10ca6d54c79533ee39f3a
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-17 20:04:42 +0530

    added new code

---
+++ Sem-1/itps/c-lang/exp-1.2-bitwise-operators.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() 
+
+{
+
+    int  a,b,c;
+
+        printf("Enter a your first number: ");
+        scanf("%d", &a);
+    
+        printf("Enter a your second number: ");
+        scanf("%d", &b);
+    
+        printf("Bitwise AND is = %d\n", a&b);
+    
+        printf("Bitwise OR is = %d\n", a|b);
+    
+        printf("Bitwise XOR is = %d\n", a^b);
+    
+        printf("Bitwise complement of first number is = %d\n", ~a);
+    
+        printf("Bitwise complement of second number is = %d\n", ~b);
+    
+        printf("Right shift of first number by 2 is = %d\n", a>>2);
+    
+        printf("Left shift of first number by 3 is = %d\n", b<<3);
+    
+    return 0;
+
+}
