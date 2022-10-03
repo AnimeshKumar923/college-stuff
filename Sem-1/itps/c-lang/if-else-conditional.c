ba0d3006cbeb2123e77bb02e3c7f6e140cdd2dbc
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-10-03 19:13:54 +0530

    new topic learned and applied

---
+++ Sem-1/itps/c-lang/if-else-conditional.c
@@ -0,0 +1,22 @@
+// program to check whether a number is even or odd
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int a,b;
+  printf("Enter a number:\n");
+  scanf("%d", &a);
+
+  if(a%2==0)
+  {
+    printf("%d is even.\n", a);
+  }
+
+  else
+    {
+    printf("%d is odd.\n", a);
+    }
+  
+  return 0;
+}
