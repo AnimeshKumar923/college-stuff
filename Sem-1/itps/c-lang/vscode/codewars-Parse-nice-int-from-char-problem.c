51d1e22773f3e7476edf753ec075df4c22c9c8d1
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-11 00:21:58 +0530

    several files added

---
+++ Sem-1/itps/c-lang/vscode/codewars-Parse-nice-int-from-char-problem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("how old are you? \nenter your age: ");
+    scanf("%d", &age);
+
+    printf("%d years old", age);
+
+    return 0;
+}
