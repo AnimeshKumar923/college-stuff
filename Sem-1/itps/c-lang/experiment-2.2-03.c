543f4bb6bc8d5d539a2b54e1e1ce682676d07e14
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-15 20:13:05 +0530

    created program for practical 5.3

---
+++ Sem-1/itps/c-lang/experiment-2.2-03.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100];
+    int size, i, search, flag;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    printf("Enter elements in array: ");
+    
+    for (i=0; i<size; i++)
+    
+        scanf("%d", &arr[i]);
+
+    printf("\nEnter element to search: ");
+    scanf("%d", &search);
+
+    flag=0;
+
+    for (i=0; i<size; i++)
+    {
+        if (arr[i]==search)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (flag==1)
+        printf("\n%d is found at position %d", search, i+1);
+
+    else
+        printf("\n%d is not found in the array", search);
+    
+
+    return 0;
+}
