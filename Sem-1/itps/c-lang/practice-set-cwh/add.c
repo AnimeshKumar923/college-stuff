fad6b2305f768d048195a7df2024a5b9f1786d3a
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-16 19:21:47 +0530

    organized files, completed practical-02

---
+++ Sem-1/itps/c-lang/practice-set-cwh/add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    
+    printf("enter number a: ");
+    scanf("%d", &a);
+    
+    printf("enter number b: ");
+    scanf("%d", &b);
+
+    printf("sum of both numbers is: %d", a+b);
+
+    return 0;
+}
