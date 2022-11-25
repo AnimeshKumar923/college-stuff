9eacdab9e7db815eb0379656a7f7e0d762a64b89
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-25 09:51:59 +0530

    learned about goto statement

---
+++ Sem-1/itps/c-lang/practice-set-cwh/c-tut-17-goto-statement.c
@@ -0,0 +1,37 @@
+/*Author: Animesh Kumar
+Purpose: Useing "goto" statement.
+Date: 19-11-2022
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // label:
+    //     printf("we are inside label");
+    //     goto end;
+    // printf("Hello World\n");
+    // goto label;
+    // end:
+    //     printf("we are at end");
+
+    int num;
+    for(int i = 0; i < 8; i++)
+    {
+        printf("%d\n", i);
+        for(int j = 0; j < 8; j++)
+        {
+            printf("Enter the number. enter 0 to exit\n");
+            scanf("%d", &num);
+            if(num==0){
+                goto end;
+            }
+        }
+        
+    }
+    end:
+        printf("exiting program. BYE!");
+    
+
+    return 0;
+}
