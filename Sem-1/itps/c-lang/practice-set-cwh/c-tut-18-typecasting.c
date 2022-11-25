d4ffd8c364f7b61df8531c1457d8cce653812ab7
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-25 09:53:27 +0530

    learn about typecasting, little confused about it

---
+++ Sem-1/itps/c-lang/practice-set-cwh/c-tut-18-typecasting.c
@@ -0,0 +1,17 @@
+/*Author: Animesh Kumar
+Purpose: Implementing typecasting.
+Date: 19-11-2022
+*/
+
+#include <stdio.h>
+// Typecasting Syntax
+// (type) value;
+int main()
+{
+
+	int a = 3;
+	float b= (float)54/5;
+	printf("The Value of a is %f\n", b );
+	return 0;
+
+}
