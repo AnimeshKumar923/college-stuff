ce9fd616bc90dad6ef491713cb8837bcd1455185
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-27 15:48:58 +0530

    learn functions, apply concept

---
+++ Sem-1/itps/c-lang/practice-set-cwh/c-tut-19-functions.c
@@ -0,0 +1,42 @@
+/*Author: Animesh Kumar
+Purpose: Learning about functions
+Date: 27-11-2022
+Status: Copy-paste with understanding
+*/
+
+#include <stdio.h>
+
+int sum(int a, int b);
+void printstar(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%c", '*');
+    }
+}
+
+int takenumber()
+{
+    int i;
+    printf("Enter a number: ");
+    scanf("%d", &i);
+    return i;
+}
+
+int main()
+{
+    int a, b, c;
+    a = 9;
+    b = 87;
+    // c = sum(a, b);
+    // printstar(7);
+    c = takenumber();
+    // printf("The sum is %d \n", c);
+    printf("The number entered is %d \n", c);
+    return 0;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
