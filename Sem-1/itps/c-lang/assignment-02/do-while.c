c5b25636c675295b10464961188feba0f316c8cd
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-12-04 16:10:23 +0530

    assignment-02

---
+++ Sem-1/itps/c-lang/assignment-02/do-while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+        int i;
+        i=0;
+
+        do
+        {
+                printf("hello\n");
+                i++;
+        }while(i<2);
+
+        return 0;
+
+}
+
---
+++ Sem-1/itps/c-lang/assignment-02/while-do-while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	i=0;
+
+	while(i<2)
+	{
+		printf("hello\n");
+		i++;
+	}
+
+	return 0;
+
+}
