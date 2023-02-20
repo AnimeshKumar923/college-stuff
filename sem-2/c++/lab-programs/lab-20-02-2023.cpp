8b66a6317b4e12abe71ee17df08a555245acb387
Animesh Kumar <kumaranimesh923@gmail.com>
2023-02-20 14:22:40 +0530

    classes and objects application

---
+++ sem-2/c++/lab-programs/lab-20-02-2023.cpp
@@ -0,0 +1,17 @@
+/*
+Aim: - Create a class called employee with the following details as variables within it.
+
+1. Name of the employee (string)
+
+2. Age (int)
+
+3. Designation (string)
+
+4. Salary (double)
+
+Write a program to create array of objects for the same to access these. Also, make use of member functions to accept values and print the name, age, designation and salary.
+*/
+
+#include <iostream>
+
+using namespace std;
