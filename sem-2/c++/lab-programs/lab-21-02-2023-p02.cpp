0aa55348b00b37de127d2a8eed2fbede9e318d23
Animesh Kumar <kumaranimesh923@gmail.com>
2023-02-21 15:44:28 +0530

    Create lab-21-02-2023-p02.cpp

---
+++ sem-2/c++/lab-programs/lab-21-02-2023-p02.cpp
@@ -0,0 +1,25 @@
+/*Aim: - WAP to illustrate the use of scope resolution operator. Display the various values of the same variables declared at different scope levels.
+*/
+
+#include <iostream>
+using namespace std;
+
+class demo{
+    int a,b;
+    
+    public:
+    
+    void getdata(int,int); //declare member function
+    void display(); //declare member function
+};
+
+int main() {
+    demo obj;
+    obj.getdata(2,30);
+    obj.display();
+    return 0;
+}
+
+void::
+    
+}
