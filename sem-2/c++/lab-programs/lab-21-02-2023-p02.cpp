3aa2521c3822d48f448cc3373fa7a2abd3866fe8
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-26 18:33:34 +0530

    code fix

--- sem-2/c++/lab-programs/lab-21-02-2023-p02.cpp
+++ sem-2/c++/lab-programs/lab-21-02-2023-p02.cpp
@@ -1,25 +1,24 @@
 /*Aim: - WAP to illustrate the use of scope resolution operator. Display the various values of the same variables declared at different scope levels.
-*/
+ */
 
 #include <iostream>
 using namespace std;
 
-class demo{
-    int a,b;
-    
-    public:
-    
-    void getdata(int,int); //declare member function
-    void display(); //declare member function
+class demo
+{
+    int a, b;
+
+public:
+    void getdata(int, int); // declare member function
+    void display();         // declare member function
 };
 
-int main() {
+int main()
+{
     demo obj;
-    obj.getdata(2,30);
+    obj.getdata(2, 30);
     obj.display();
     return 0;
 }
 
-void::
-    
-}
+void ::
--- sem-2/c++/lab-programs/program-01.cpp
+++ sem-2/c++/lab-programs/program-01.cpp
@@ -1,6 +1,6 @@
 
-# include <iostream>
-# include <math.h>
+#include <iostream>
+#include <math.h>
 
 // WAP to swap first and last digit of a number , i.e. (1234 , 4231).
 
@@ -8,31 +8,32 @@ using namespace std;
 
 int swap(int num);
 
-int main() {
-    int num, sum=0;
+int main()
+{
+    int num, sum = 0;
 
-    cout<<"Enter a number = ";
-    cin>>num;
-    
-    cout<<"The swaped number = "<<swap(num);
+    cout << "Enter a number = ";
+    cin >> num;
+
+    cout << "The swaped number = " << swap(num);
 
     return 0;
 }
 
 int swap(int num)
 {
-    int sum=0;
+    int sum = 0;
 
     int no_digits = (int)log10(num);
 
-    int first_digit = num / (int)pow(10,no_digits);    // For the first digit.
+    int first_digit = num / (int)pow(10, no_digits); // For the first digit.
 
-    int last_digit = num % 10;       // For the last digit.
+    int last_digit = num % 10; // For the last digit.
 
-    int extra = num % (int)pow(10,no_digits);
-    num = extra / 10;                            // for the rest of the digits left in the middle.
+    int extra = num % (int)pow(10, no_digits);
+    num = extra / 10; // for the rest of the digits left in the middle.
 
-    int new_num = ( last_digit * (int)pow(10,no_digits) ) + (num*10) + ( first_digit ); 
+    int new_num = (last_digit * (int)pow(10, no_digits)) + (num * 10) + (first_digit);
 
     return new_num;
 }
