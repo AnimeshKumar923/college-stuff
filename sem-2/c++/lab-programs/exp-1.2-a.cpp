b41bb5a50a90682b52ea08b325d8ef7a195ea3b3
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-02-26 22:33:07 +0530

    add exp-1.2-a

---
+++ sem-2/c++/lab-programs/exp-1.2-a.cpp
@@ -0,0 +1,16 @@
+/*Aim: - WAP to illustrate the use of scope resolution operator. Display the various values of the same variables declared at different scope levels.
+ */
+
+#include <iostream>
+using namespace std;
+
+int a = 10; // global variable
+int main()
+{
+
+    int a = 20; // local variable
+    cout << "value of local variable a is: " << a << "\n";
+    cout << "value of global variable a is: " << a << "\n";
+
+    return 0;
+}
