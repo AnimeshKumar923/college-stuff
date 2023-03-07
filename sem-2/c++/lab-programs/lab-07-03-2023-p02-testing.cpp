944fe95ec37eca23566cee15b14b7d963642bbc7
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-07 16:19:30 +0530

    test some files, concepts

---
+++ sem-2/c++/lab-programs/lab-07-03-2023-p02-testing.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+class A;
+class B;
+
+void swap(A obj1, B obj2);
+
+class A
+{
+    int a;
+
+public:
+    A()
+    {
+        cout << "Enter A: ";
+        cin >> a;
+    }
+
+    friend void swap(A obj1, B obj2);
+};
+
+class B
+{
+    int b;
+
+public:
+    B()
+    {
+        cout << "Enter B: ";
+        cin >> b;
+    }
+
+    friend void swap(A obj1, B obj2);
+};
+
+int main()
+{
+    B obj2;
+    A obj1;
+
+    swap(obj1, obj2);
+
+    return 0;
+}
+
+void swap(A obj1, B obj2)
+{
+    obj1.a = obj1.a + obj2.b;
+    obj2.b = obj1.a - obj2.b;
+    obj1.a = obj1.a - obj2.b;
+
+    cout << "a = " << obj1.a << endl;
+    cout << "b = " << obj2.b << endl;
+}
