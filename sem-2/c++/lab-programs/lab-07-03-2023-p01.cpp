2702d3fa19f8a8786f2f0c91ace0ab2fed0398f7
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-07 16:02:07 +0530

    lab program done, swap two numbers

---
+++ sem-2/c++/lab-programs/lab-07-03-2023-p01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class A;
+class B
+{
+
+}
+
+class B
+{
+    int x;
+
+public:
+    A()
+    {
+        cout << "Enter a number: ";
+        cin >> x;
+    }
+}
+
+int
+main()
+{
+    ;
+    return 0;
+}
