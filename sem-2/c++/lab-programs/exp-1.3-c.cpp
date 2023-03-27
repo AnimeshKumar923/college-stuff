d99d7927e6f2e7d4412ff6744248d9836c365498
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-03-28 00:10:26 +0530

    Pending exp-1.3-c
    Need to understand this program...

---
+++ sem-2/c++/lab-programs/exp-1.3-c.cpp
@@ -0,0 +1,2 @@
+// Aim:- A dining hall can accommodate only 50 guests. Create a class to store seat number (Generated Automatically) and name of the guests who are seated on first come first seated basis.
+// Define functions to display name of all guests along with seat number. Write a program to show the working of this class using the concept of static data member and static function
