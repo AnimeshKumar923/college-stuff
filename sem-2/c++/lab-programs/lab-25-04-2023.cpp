dce292211c23c3e7626b3d034dcf9fa9404dcfc7
Animesh Kumar <kumaranimesh923@gmail.com>
2023-04-25 16:23:46 +0545

    Runtime polymorphism, exp-7.3, inprogress
    Aim: - WAP to design a class representing the information regarding digital
    library (books, tape: book & tape should be separate classes having the base
    class as media).The class should have the functionality for adding new item,
    issuing, deposit etc. The program should link the objects with concerned
    function by the concept of runtime polymorphism

---
+++ sem-2/c++/lab-programs/lab-25-04-2023.cpp
@@ -0,0 +1,94 @@
+/*
+
+Aim: - WAP to design a class representing the information regarding digital
+library (books, tape: book & tape should be separate classes having the base
+class as media).The class should have the functionality for adding new item,
+issuing, deposit etc. The program should link the objects with concerned
+function by the concept of runtime polymorphism
+
+*/
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class media{
+protected:
+  string title;
+  float price;
+
+public:
+  media(char* s, float p){
+    strcpy(title,s);
+    price=p;
+  }
+
+virtual void disp(){}
+};
+
+class book:public media{
+int pages;
+public:
+  book(char* s, float a, int p):media(s,a){
+    pages=p;
+  }
+
+void disp();
+};
+
+class tape:public media{
+float time;
+public:
+  tape(char* s, float a, float t):media(s,a){
+    time=t;
+  }
+
+void disp();
+};
+
+void book::disp(){
+  cout<<"\nTitle: "<<title;
+  cout<<"\nPages: "<<pages;
+  cout<<"\nPrice: "<<price;
+}
+
+void tape::disp(){
+  cout<<"\nTitle: "<<title;
+  cout<<"\nPlay-time: "<<time<<"mins";
+  cout<<"\nPrice: "<<price;
+}
+
+int main() {
+  char* title=new char[30];
+  float price,time;
+  int pages;
+
+  cout<<"\nEnter book details: \n";
+  cout<<"\nTitle: ";
+  cin>>title;
+  cout<<"\nPrice: ";
+  cin>>price;
+  cout<<"\nPages: ";
+  cin>>pages;
+
+  // book b1(title,price,pages);
+
+  cout<<"\nEnter Tape details: \n";
+  cout<<"\nTitle: ";
+  cin>>title;
+  cout<<"\nPrice: ";
+  cin>>price;
+  cout<<"\mPlay-time(mins): ";
+  cin>>time;
+  // tape t1(title,price,time);
+
+
+  media* ptr1;
+  media* ptr2;
+  
+  ptr1=new book(title,price,pages);
+  ptr2=new tape(title,price,time);
+
+  ptr1->disp();
+  ptr2->disp();
+}
