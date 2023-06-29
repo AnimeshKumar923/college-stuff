ba1792d978f625b4e387851f2dd9627d7d741db5
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-06-29 13:40:32 +0530

    sem-3 content started, blockchain

---
+++ sem-3/vac-blockchain/code.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+    function kiran() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
