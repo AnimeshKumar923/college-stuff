ec52d97f5b1dbea3635085f9c1b7d7360269a10b
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-08-07 14:17:11 +0530

    array deletion program

---
+++ sem-3/data-structures/src/Lab01_03.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Lab01_03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter size of array:");
+        int size = in.nextInt();
+
+        int[] arr = new int[size];
+        arr = new int[50];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        // for (int i = 0; i < size; i++) {
+        // System.out.print(arr[i] + " ");
+        // }
+
+        System.out.println("\n");
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("Enter position to enter new element: ");
+        int position = in.nextInt();
+
+        for (int i = position - 1; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        size--;
+        System.out.println("New array = " + Arrays.toString(arr));
+    }
+}
