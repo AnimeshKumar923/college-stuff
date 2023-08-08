45d10f4ebe08b7dcd32fcafcdd6dac912bcb3548
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-08-08 19:53:22 +0530

    rename file

--- sem-3/data-structures/src/Lab01_01.java
+++ sem-3/data-structures/src/Lab01_01.java
@@ -5,7 +5,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Lab01 {
+public class Lab01_01 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
