8dc0b5513cadae1a59882c51e9a25c7af4ca9589
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-08-27 23:48:39 +0530

    modify binary search code

--- sem-3/java-lab/src/BinarySearch.java
+++ sem-3/java-lab/src/BinarySearch.java
@@ -19,11 +19,10 @@ public class BinarySearch {
             arr[i] = in.nextInt();
         }
 
-        bubbleSort(size, arr);
+        // bubbleSort(size, arr);
 
-    }
+        // binarySearch(size, arr);
 
-    static void bubbleSort(int size, int arr[]) {
         for (int i = 0; i < size; i++) {
             for (int j = 1; j < size - i; j++) {
                 int flag = 0;
@@ -44,5 +43,74 @@ public class BinarySearch {
             System.out.print(arr[i] + " ");
 
         }
+
+        System.out.println("\n\nEnter element to search: ");
+        int target = in.nextInt();
+
+        int start = 0;
+        int end = size - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (target == arr[mid]) {
+                System.out.println("Element found at " + mid + "th index");
+                break;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+
+            else {
+                end = mid + 1;
+            }
+        }
+
     }
+
+    // static void bubbleSort(int size, int arr[]) {
+    // for (int i = 0; i < size; i++) {
+    // for (int j = 1; j < size - i; j++) {
+    // int flag = 0;
+    // if (arr[j] < arr[j - 1]) {
+    // int temp = arr[j];
+    // arr[j] = arr[j - 1];
+    // arr[j - 1] = temp;
+
+    // flag = 1;
+    // }
+    // if (flag == 0) {
+    // break;
+    // }
+    // }
+    // }
+
+    // for (int i = 0; i < size; i++) {
+    // System.out.print(arr[i] + " ");
+
+    // }
+    // }
+
+    // static void binarySearch(int size, int arr[]) {
+    // Scanner in = new Scanner(System.in);
+    // System.out.println("\n\nEnter element to search: ");
+    // int target = in.nextInt();
+
+    // int start = 0;
+    // int end = size - 1;
+
+    // while (start <= end) {
+    // int mid = (start + end) / 2;
+
+    // if (target == arr[mid]) {
+    // System.out.println("Element found at " + mid + "th index");
+    // break;
+    // } else if (target > arr[mid]) {
+    // start = mid + 1;
+    // }
+
+    // else {
+    // end = mid + 1;
+    // }
+    // }
+    // }
 }
