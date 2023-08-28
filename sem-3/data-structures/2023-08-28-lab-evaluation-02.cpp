2156842655f2d44dfbaee3c18f030e3fabba1390
Animesh Kumar <kumaranimesh923@gmail.com>
2023-08-28 11:09:12 +0530

    converted to functional code

---
+++ sem-3/data-structures/2023-08-28-lab-evaluation-02.cpp
@@ -0,0 +1,84 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int max(int arr[], int size);
+int min(int arr[], int size);
+
+void sum(int max, int min);
+void product(int max, int min);
+
+int main() {
+    // Write C++ code here
+    int size;
+    
+    cout<<"Enter array size: ";
+    cin>>size;
+    
+    int arr[size];
+    
+    cout<<"Enter array elements: "<<endl;
+    
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+    
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    
+    
+    int maxim=max(arr, size);
+    cout<<"\nmax element: "<<maxim;
+    int minim=min(arr, size);
+    cout<<"\nmin element: "<<minim;
+
+  
+    
+    sum(maxim, minim);
+    product(maxim, minim);
+  
+  
+    return 0;
+}
+
+void sum(int max, int min){
+    cout<<"\nsum = "<<(max+min);
+}
+
+void product(int max, int min){
+    cout<<"\nproduct = "<<(max*min);
+}
+
+int max(int arr[], int size){
+    
+    int max=arr[0];
+    
+    
+    for(int i=0; i<size; i++){
+        
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    
+    return max;
+
+}
+
+int min(int arr[], int size){
+    
+    int min=arr[0];
+    
+    for(int i=0; i<size; i++){
+        
+        if(arr[i]<min){
+            
+        min=arr[i];
+        }
+    }
+    
+    
+    return min;
+}
