859d824cb2694d683b21364f9426556d15feee68
Animesh Kumar <kumaranimesh923@gmail.com>
2023-09-04 05:26:03 +0000

    code update, linked list

--- sem-3/data-structures/2023-09-04-lab-03-01-linked-list.cpp
+++ sem-3/data-structures/2023-09-04-lab-03-01-linked-list.cpp
@@ -12,38 +12,49 @@ class node
             next=NULL;
         }
 
-        void insertLast(node* &head, int value){
-            node* n=new node(value);
-
-            if(head==NULL){
-                head=n;
-            }
-
-            node* temp=head;
-            while(temp->next!=NULL){
-                temp=temp->next;
-            }
-
-            temp->next=n;
-        }
-
-        void disp(node* head){
-            node* temp=head;
-            while (temp!=NULL)  
-            {
-                cout<<temp->data<<" ";
-                temp=temp->next;
-            }
-
-            cout<<endl;
-            
+        node(){
+            data=0;
+            next=NULL;
         }
 };
+
+void insertLast(node* &head, int value){
+    node* n=new node(value);
+
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+
+    temp->next=n;
+}
+
+void disp(node* head){
+    node* temp=head;
+    while (temp!=NULL)  
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+
+    cout<<endl;
+    
+}
 
 
 int main()
 {
     node* head=NULL;
-    
+    insertLast(head,254);
+    insertLast(head,125);
+    insertLast(head,541);
+    insertLast(head,6969);
+    disp(head);
+
     return 0;
 }
