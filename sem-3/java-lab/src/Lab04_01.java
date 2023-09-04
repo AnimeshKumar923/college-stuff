a289b0a08b0c4c8cbe18a975815364a87b8e5648
Animesh Kumar <kumaranimesh923@gmail.com>
2023-09-04 08:44:32 +0000

    inheritance concepts, this, super

---
+++ sem-3/java-lab/src/Lab04_01.java
@@ -0,0 +1,25 @@
+public class Lab04_01 {
+    public static void main(String[] args){
+
+        B x1=new B();
+        x1.disp(98);
+    }
+
+    static class A {
+    
+        protected String name;
+        protected int age;
+        int a=245;
+    }
+
+    static class B extends A{
+        int a=45;
+
+        void disp(int a){
+            System.out.println(a);
+            System.out.println(this.a);
+            System.out.println(super.a);
+        }
+    }
+
+}
