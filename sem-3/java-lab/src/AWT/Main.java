200811bd8f498353f4286c95d55e375925ca2268
Animesh Kumar <kumaranimesh923@gmail.com>
2023-10-18 08:51:33 +0000

    Java AWT practical

---
+++ sem-3/java-lab/src/AWT/Main.java
@@ -0,0 +1,12 @@
+package AWT;
+
+import java.awt.*;
+public class Main {
+    public static void main(String[] args) {
+        Frame frame=new Frame();
+
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
