bb26b2bda0e0b15aa1c3fb129a0e3bc2c0afd1b9
Animesh Kumar <kumaranimesh923@gmail.com>
2023-10-23 04:45:43 +0000

    Stack using LinkedList

---
+++ sem-3/java-lab/src/dataStructure/stack.java
@@ -0,0 +1,14 @@
+package dataStructure;
+
+import java.util.*;
+
+
+public class stack {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        Queue<String> q = new LinkedList<String>();
+
+        
+    }
+}
