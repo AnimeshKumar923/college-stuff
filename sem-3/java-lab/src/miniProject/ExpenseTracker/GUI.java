05602a0a9c4e4ef4dbea3acb57547a2b336a90c9
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-11-02 02:14:47 +0530

    fix: add functionality to code
    fixed where the two fields are separate.

--- sem-3/java-lab/src/miniProject/ExpenseTracker/GUI.java
+++ sem-3/java-lab/src/miniProject/ExpenseTracker/GUI.java
@@ -7,44 +7,66 @@ import java.awt.event.ActionListener;
 import javax.swing.table.DefaultTableModel;
 
 public class GUI {
-    public static void main(String[] args) {
+    private DefaultTableModel tableModel;
+    private JTextField expenseNameField;
+    private JTextField expenseAmountField;
+
+    public GUI() {
         JFrame frame = new JFrame("Expense Tracker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900, 600);
+        frame.setSize(400, 300);
 
         JPanel panel = new JPanel();
         frame.add(panel);
         panel.setLayout(new BorderLayout());
 
-        JTextField expenseNameField = new JTextField();
-        JTextField expenseAmountField = new JTextField();
-        JButton addButton = new JButton("Add Expense");
+        expenseNameField = new JTextField();
+        expenseAmountField = new JTextField();
 
-        DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel = new DefaultTableModel();
         tableModel.addColumn("Expense Name");
         tableModel.addColumn("Amount");
         JTable expenseTable = new JTable(tableModel);
 
         JScrollPane scrollPane = new JScrollPane(expenseTable);
 
-        panel.add(expenseNameField, BorderLayout.NORTH);
-        panel.add(expenseAmountField, BorderLayout.CENTER);
-        panel.add(addButton, BorderLayout.SOUTH);
-        panel.add(scrollPane, BorderLayout.AFTER_LAST_LINE);
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(2, 2));
+        inputPanel.add(new JLabel("Expense Name:"));
+        inputPanel.add(expenseNameField);
+        inputPanel.add(new JLabel("Amount:"));
+        inputPanel.add(expenseAmountField);
+
+        // Create a new JPanel for the buttons
+        JPanel buttonPanel = new JPanel();
+        JButton addButton = new JButton("Add Expense");
+        buttonPanel.add(addButton);
+
+        panel.add(inputPanel, BorderLayout.NORTH);
+        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
 
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String name = expenseNameField.getText();
-                String amount = expenseAmountField.getText();
-                tableModel.addRow(new Object[]{name, amount});
-                // Add code to save the data to the database here
-                // You'll do this in Step 4
-                expenseNameField.setText("");
-                expenseAmountField.setText("");
+                addExpense();
             }
         });
 
         frame.setVisible(true);
+    }
+
+    private void addExpense() {
+        String name = expenseNameField.getText();
+        String amount = expenseAmountField.getText();
+        tableModel.addRow(new Object[]{name, amount});
+        // Add code to save the data to the database here
+        // You'll do this in Step 4
+        expenseNameField.setText("");
+        expenseAmountField.setText("");
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GUI());
     }
 }
