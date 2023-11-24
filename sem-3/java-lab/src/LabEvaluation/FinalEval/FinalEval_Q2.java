6a5fd7fb68335006387fcb606f79f7d681140506
Animesh Kumar <kumaranimesh923@gmail.com>
2023-11-24 05:17:50 +0000

    lab evaluation question 02

--- sem-3/java-lab/src/LabEvaluation/FinalEval/FinalEval_Q2.java
+++ sem-3/java-lab/src/LabEvaluation/FinalEval/FinalEval_Q2.java
@@ -5,27 +5,63 @@ import java.io.*;
 public class FinalEval_Q2 {
    static public class Sum implements Serializable{
         private int a;
-        private int b;
-        private int result;
+        private String b;
 
-
-        public void sum(int a, int b){
-            a=this.a;
-            b=this.b;
-            this.result=a+b;
+        public Sum(int a, String b){
+            this.a=a;
+            this.b=b;
         }
 
-        public void disp(){
-            System.out.println("Sum of a & b = "+result);
-        }
+       
+
     }
 
 
-    public static void main(String[] args) {
-        Sum x1=new Sum();
+    public static void main(String[] args) throws Exception {
+        Sum x1=new Sum(10, "Animesh");
+        String filename = "file.ser";
 
-        x1.sum(10, 20);
-        x1.disp();
+
+
+        try
+        {   
+            FileOutputStream f = new FileOutputStream(filename);
+            ObjectOutputStream o = new ObjectOutputStream(f);
+             
+            o.writeObject(x1);
+             
+            o.close();
+            f.close();
+             
+            System.out.println("Object has been serialized");
+ 
+        }
+         
+        catch(IOException ex)
+        {
+            System.out.println("IOException is caught");
+        }
+
+        try
+        {   
+            FileInputStream f = new FileInputStream(filename);
+            ObjectInputStream i = new ObjectInputStream(f);
+             
+            Sum x2=null;
+            x2 = (Sum)i.readObject();
+             
+            i.close();
+            f.close();
+             
+            System.out.println("Object has been deserialized ");
+            System.out.println("a = " + x2.a);
+            System.out.println("b = " + x2.b);
+        }
+         
+        catch(IOException ex)
+        {
+            System.out.println("IOException is caught");
+        }
 
     }
 }
