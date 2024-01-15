cd655c5f50669445ee128a6c8a9068567d7a1871
Animesh Kumar <kumaranimesh923@gmail.com>
2024-01-15 12:01:43 +0530

    sem-04 start, python lab

---
+++ sem-4/python/lab-01-2024-01-15.py
@@ -0,0 +1,23 @@
+# a=10;
+# print(a);
+
+# b='''
+#     This is a multi-line string.
+#     This is the second line.
+#     "What's your name?," I asked.
+#     He said "Bond, James Bond."
+# ''';
+
+# print(b);
+# print(len(b));
+
+a=10;
+b=20;
+
+print("a+b=" + str(a) + str(b));
+print(a-b);
+print(a*b);
+print(a/b);
+print(a**b);
+print(a%b);
+
