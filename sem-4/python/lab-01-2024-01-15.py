1157e4fdc2bd83cc93079c63b36217a3414df410
Animesh Kumar <kumaranimesh923@gmail.com>
2024-01-23 22:03:11 +0530

    python lab files, basics of python done

--- sem-4/python/lab-01-2024-01-15.py
+++ sem-4/python/lab-01-2024-01-15.py
@@ -1,6 +1,25 @@
 # a=10;
 # print(a);
 
+# print(len(b));
+
+# a=10;
+# b=20;
+
+# print("a+b=" + str(a) + str(b));
+# print(a-b);
+# print(a*b);
+# print(a/b);
+# print(a**b);
+# print(a%b);
+
+# c="hello world"
+# length=len(c);
+# print(length);
+
+# for i in range(0,length):
+#   print(c[i]);
+
 # b='''
 #     This is a multi-line string.
 #     This is the second line.
@@ -9,15 +28,12 @@
 # ''';
 
 # print(b);
-# print(len(b));
 
-a=10;
-b=20;
+# a="hello";
+# print((a+" world\n")*4);
 
-print("a+b=" + str(a) + str(b));
-print(a-b);
-print(a*b);
-print(a/b);
-print(a**b);
-print(a%b);
+# l=[1,2,"hello", 5.2];
+# l.append("world");
 
+# for i in range(0, len(l)):
+#   print(l[i]);
---
+++ sem-4/python/lab-02-2024-01-22.py
@@ -0,0 +1,31 @@
+# a="This is";
+# b=a+" a String"
+# print(b)
+
+# c="c\new\temp";
+# d=R"c\new\temp";
+
+# print(c)
+# print(d)
+
+# a="HELLO"
+
+# for i in range(0, len(a)):
+#   print(a[i]);
+
+# b="diwdmiwninenvnwnfew nef iwen ine s"
+# print(b[:20])
+
+# l=[1,2,3.5,"string"];
+# for i in range(0, len(l)):
+#   print(l[i]);
+
+l2=["dsd","d",l]
+# print(l2)
+
+a=(2,3,5.2,"String")
+print(type(a))
+print(a)
+
+d1={1:"one", 2:"two", 3:"three"}
+print(d[2]);
