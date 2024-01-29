6cf431a8b8179f9774d7539e3b97324a5ce7d693
Animesh Kumar <kumaranimesh923@gmail.com>
2024-01-29 22:27:06 +0530

    python lists, dictonaries

---
+++ sem-4/python/lab-03-2024-01-29.py
@@ -0,0 +1,14 @@
+list=[1,2,2.5,2.6547, "String", "name"];
+print(list);
+list;
+
+dict={"one":"rupee", "two":"dollar", "three":'ruble', "four":"taka"};
+
+print(dict[("one")]);
+
+a=input("enter first number: ");
+b=input("enter first number: ");
+
+if a>b:
+    if b>c:
+        print(b)
