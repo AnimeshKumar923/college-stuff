804752ef7adb97a72621b6248a039ab3762dad5a
Animesh Kumar <kumaranimesh923@gmail.com>
2024-02-22 20:02:09 +0530

    lab evaluation-01

---
+++ sem-4/python/lab-04-2024-02-05-eval-01.py
@@ -0,0 +1,21 @@
+# fizzBuzz
+
+for i in range(1,50):
+    if(i%3==0 and i%5==0):
+        print("fizzBuzz")
+        print(i)
+    elif(i%3==0):
+        print("fizz")
+        print(i)
+    elif(i%5==0):
+        print("Buzz")
+        print(i)
+
+# target=input();
+print("  *****  ")
+print(" *     * ")
+print("*      ")
+print("*  * * * ")
+print("*      * ")
+print(" *     * ")
+print("  *****  ")
