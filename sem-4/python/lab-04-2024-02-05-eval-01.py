ae3803090e4c3b4ac041fd427e2328ea203ebc92
Animesh Kumar <kumaranimesh923@gmail.com>
2024-02-22 20:08:50 +0530

    add question statement, lab evaluation-01

--- sem-4/python/lab-04-2024-02-05-eval-01.py
+++ sem-4/python/lab-04-2024-02-05-eval-01.py
@@ -1,4 +1,11 @@
-# fizzBuzz
+# QUESTION 1:
+# Write a short program that prints each number from 1 to 100 on a new line. 
+
+# For each multiple of 3, print "Fizz" instead of the number. 
+
+# For each multiple of 5, print "Buzz" instead of the number. 
+
+# For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
 for i in range(1,50):
     if(i%3==0 and i%5==0):
@@ -11,7 +18,9 @@ for i in range(1,50):
         print("Buzz")
         print(i)
 
-# target=input();
+# QUESTION 2:
+# Print the alphabet "G" using asterisk in python
+        
 print("  *****  ")
 print(" *     * ")
 print("*      ")
