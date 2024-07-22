39910df3caf10ef4bfec15cdbd2b7e0091e4a26b
Animesh Kumar <kumaranimesh923@gmail.com>
2024-07-22 22:17:10 +0530

    add ap lab file, sem 5 start

---
+++ sem-5/ap-lab/exp-01.js
@@ -0,0 +1,15 @@
+const countriesInput = `
+USA
+Canada
+Mexico
+Brazil
+Argentina
+`;
+
+const countries = countriesInput.trim().split('\n');
+
+const realIndex = 3;
+const atZero = countries[realIndex];
+
+console.log(atZero);
+
